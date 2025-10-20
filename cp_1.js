bc0b8de48a1a0cefa246c827eecfe7b93d31bcb8
Pravallika <pkathi@usf.edu>
2025-10-19 21:33:46 -0400

    event listener update counters as user types

--- cp_1.js
+++ cp_1.js
@@ -9,3 +9,14 @@ const commentsCount = document.createElement("p");
 nameInput.insertAdjacentElement("afterend", nameCount);
 emailInput.insertAdjacentElement("afterend", emailCount);
 commentsInput.insertAdjacentElement("afterend", commentsCount);
+
+nameInput.addEventListener("input", () => {
+  nameCount.textContent = `Characters: ${nameInput.value.length}`;
+});
+emailInput.addEventListener("input", () => {
+  emailCount.textContent = `Characters: ${emailInput.value.length}`;
+});
+commentsInput.addEventListener("input", () => {
+  commentsCount.textContent = `Characters: ${commentsInput.value.length}`;
+});
+
