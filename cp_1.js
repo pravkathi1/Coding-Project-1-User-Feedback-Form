2d87e8cb2998b130e8636f997c4bf614ce7e2208
Pravallika <pkathi@usf.edu>
2025-10-19 21:32:11 -0400

    elements character count

--- cp_1.js
+++ cp_1.js
@@ -2,3 +2,7 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const commentsInput = document.getElementById("comments");
 
+const nameCount = document.createElement("p");
+const emailCount = document.createElement("p");
+const commentsCount = document.createElement("p");
+
